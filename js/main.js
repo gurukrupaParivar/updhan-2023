0de3f1c19b614e8718f7ff133d47ce03cc7fdf52
Nirmal Shah <nir.shah@outlook.com>
2022-12-04 23:13:45 +0530

    script url change

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@
 });
 
 // Sending form data to google spreadsheet
-const scriptURL = 'https://script.google.com/macros/s/AKfycbx43YRa0XVUUTPoJpf_waZN9dR5_p1jU8PZr7RpDH3vydfsMNzrXfVKsHbh_n1uHGh3/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxqfBZ58mpjFO4HELpCSRnEgV7Zx4-5CGZTjOyuRhPHjnTYe_5iybcpjVa5sk6M0aRi/exec'
 const form = document.forms['register-form']
 
   form.addEventListener('submit', e => {
     e.preventDefault()
 
-    fetch(scriptURL, {method: 'POST', body: new FormData(form)})
+    fetch(scriptURL, { mode:'no-cors', method: 'POST', body: new FormData(form)})
       .then((response) => {
         if(!alert("ફોર્મ સફળતાપૂર્વક રજીસ્ટર થયું..! અમે ટૂંક સમયમાં તમારો સંપર્ક કરીશું..!")){
           location.reload(true);
